caee858d79f6af0db711ac528fefcbb5a00d0c7a
Arpit Verma <arpitv970@gmail.com>
2024-08-25 04:07:36 +0530

    created basic pieces

--- components/pieces/index.tsx
+++ components/pieces/index.tsx
@@ -1,7 +1,7 @@
 import { IPiece } from "@/lib/types";
 import React from "react";
 
-export const PieceCard: React.FC<IPiece> = ({ color, name }) => {
+export const PieceCard: React.FC<IPiece> = ({ color, name, curr }) => {
   return (
     <div
       className={`border ${color === "black" ? "bg-primary text-secondary" : "bg-secondary text-primary"} w-8 h-8 p-3 flex justify-center items-center rounded-lg`}
@@ -11,26 +11,26 @@ export const PieceCard: React.FC<IPiece> = ({ color, name }) => {
   );
 };
 
-export const Pawn: React.FC<IPiece> = ({ color, name }) => {
-  return <PieceCard color={color} name={name} />;
+export const Pawn: React.FC<IPiece> = (props) => {
+  return <PieceCard {...props} />;
 };
 
-export const Knight: React.FC<IPiece> = ({ color, name }) => {
-  return <PieceCard color={color} name={name} />;
+export const Knight: React.FC<IPiece> = (props) => {
+  return <PieceCard {...props} />;
 };
 
-export const Bishop: React.FC<IPiece> = ({ color, name }) => {
-  return <PieceCard color={color} name={name} />;
+export const Bishop: React.FC<IPiece> = (props) => {
+  return <PieceCard {...props} />;
 };
 
-export const Rook: React.FC<IPiece> = ({ color, name }) => {
-  return <PieceCard color={color} name={name} />;
+export const Rook: React.FC<IPiece> = (props) => {
+  return <PieceCard {...props} />;
 };
 
-export const Queen: React.FC<IPiece> = ({ color, name }) => {
-  return <PieceCard color={color} name={name} />;
+export const Queen: React.FC<IPiece> = (props) => {
+  return <PieceCard {...props} />;
 };
 
-export const King: React.FC<IPiece> = ({ color, name }) => {
-  return <PieceCard color={color} name={name} />;
+export const King: React.FC<IPiece> = (props) => {
+  return <PieceCard {...props} />;
 };
--- lib/types.ts
+++ lib/types.ts
@@ -2,6 +2,15 @@ export interface IColor {
   color: "black" | "white";
 }
 
-export interface IPiece extends IColor {
-  name: string;
+export interface ICoordinate {
+  rank: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8";
+  file: "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
+}
+
+export interface IName {
+  name: "P" | "N" | "B" | "R" | "Q" | "K";
+}
+
+export interface IPiece extends IColor, IName {
+  curr: ICoordinate;
 }
