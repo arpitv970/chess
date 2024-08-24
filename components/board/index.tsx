92106c7dd270391eed5f10f0513a157376ceb24f
Arpit Verma <arpitv970@gmail.com>
2024-08-24 22:12:59 +0530

    updated canvas

--- components/board/index.tsx
+++ components/board/index.tsx
@@ -2,19 +2,34 @@ import { IColor } from "@/lib/types";
 import React from "react";
 
 export const Board = () => {
-  const rows = 8;
-  const cols = 8;
+  const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  const ranks = ["8", "7", "6", "5", "4", "3", "2", "1"];
+
   return (
     <div className="w-full h-screen flex justify-center items-center m-auto">
-      <div className="border border-primary drop-shadow-xl rounded-xl overflow-hidden grid grid-cols-8 gap-0 h-[700px] w-[700px]">
-        {Array.from({ length: rows }, (r_item, r_idx) =>
-          Array.from({ length: cols }, (c_item, c_idx) => (
-            <Block
-              key={`${r_idx}${c_idx}`}
-              color={(c_idx + r_idx) % 2 === 0 ? "black" : "white"}
-            />
-          )),
-        )}
+      <div className="flex justify-center items-start">
+        <div className="flex flex-col justify-evenly items-center gap-[15px] h-[500px]">
+          {ranks.map((item, idx) => (
+            <div key={idx}>{item}</div>
+          ))}
+        </div>
+        <div className="flex flex-col justify-center items-center m-auto">
+          <div className="border border-primary drop-shadow-xl rounded-xl overflow-hidden grid grid-cols-8 gap-0 h-[500px] w-[500px]">
+            {Array.from({ length: files.length }, (r_item, r_idx) =>
+              Array.from({ length: ranks.length }, (c_item, c_idx) => (
+                <Block
+                  key={`${r_idx}${c_idx}`}
+                  color={(c_idx + r_idx) % 2 === 0 ? "black" : "white"}
+                />
+              )),
+            )}
+          </div>
+          <div className="w-[560px] mt-3 flex justify-evenly items-center">
+            {files.map((item, idx) => (
+              <div key={idx}>{item}</div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -23,7 +38,7 @@ export const Board = () => {
 export const Block: React.FC<IColor> = ({ color = "white" }) => {
   return (
     <div
-      className={`${color === "black" ? "bg-primary text-secondary" : "bg-secondary text-primary"} flex justify-center items-center m-auto h-full w-full`}
+      className={`${color === "white" ? "bg-primary text-secondary" : "bg-secondary text-primary"} flex justify-center items-center m-auto h-full w-full`}
     ></div>
   );
 };
